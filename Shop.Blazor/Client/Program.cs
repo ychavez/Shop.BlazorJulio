638a7f394a1612ef83f27625289b674d983298d6
Yael Chavez <you400@gmail.com>
2022-07-11 20:50:40 -0500

    catalog

--- Shop.Blazor/Client/Program.cs
+++ Shop.Blazor/Client/Program.cs
@@ -11,8 +11,21 @@ namespace Company.WebApplication1
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+            HttpClient http = new()
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            };
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => http);
+
+            using var response = await http.GetAsync("appsettings.json");
+            using var stream = await response.Content.ReadAsStreamAsync();
+
+            builder.Configuration.AddJsonStream(stream);
+
+
+            builder.Services.AddScoped(sp =>
+            new HttpClient { BaseAddress = new Uri(builder.Configuration["BaseAddress"]?? "/" )});
 
             await builder.Build().RunAsync();
         }
