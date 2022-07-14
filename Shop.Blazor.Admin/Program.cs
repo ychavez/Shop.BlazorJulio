e5acf0fa30efc1f38a0d750b717436d42c86a170
Yael Chavez <you400@gmail.com>
2022-07-14 12:04:01 -0500

    blazor server

---
+++ Shop.Blazor.Admin/Program.cs
@@ -0,0 +1,33 @@
+namespace Company.WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+           
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
+}
---
+++ Shop.Common/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Common.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; } = null!;
+        
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
